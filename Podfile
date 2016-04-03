f2fdedbf3ef76e1ecf2bc3725963cf89cd63eef9
rabewu <rabe.wu@gmail.com>
2016-04-03 10:00:16 +0800

    Add ReactiveCocoa into podfile

--- Podfile
+++ Podfile
@@ -3,5 +3,5 @@ platform :ios, '8.0'
 use_frameworks!
 
 target 'RWReactivePlayground' do
-pod 'AFNetworking', '~> 3.1.0'
+pod 'ReactiveCocoa', '~> 4.1.0'
 end
--- Pods/Target Support Files/Pods-RWReactivePlayground/Pods-RWReactivePlayground-frameworks.sh
+++ Pods/Target Support Files/Pods-RWReactivePlayground/Pods-RWReactivePlayground-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/ReactiveCocoa/ReactiveCocoa.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Result/Result.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/ReactiveCocoa/ReactiveCocoa.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Result/Result.framework"
 fi
